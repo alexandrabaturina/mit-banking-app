d8c9dbc372ad9e69493b9abee5eaeabaecb76be0
alexandrabaturina <alexandra.v.baturina@gmail.com>
2021-06-24 17:42:39 -0700

    feat: create home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className="card text-center">
+            <div className="card-header">
+                <h5 class="card-title">WELCOME TO THE BANK APP</h5>
+            </div>
+            <div className="card-body">
+                <p className="card-text">The Bank App is here to meet all your banking needs!</p>
+                <img src='bank.png' className='img-fluid' alt='Bad Bank' />
+            </div>
+        </div>
     )
 }
-
 export default Home
