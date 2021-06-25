2df18ed0e678ddc2eb83a9175cb5f34c1e3cd532
alexandrabaturina <alexandra.v.baturina@gmail.com>
2021-06-25 14:04:57 -0700

    feat: add input validation

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -1,5 +1,16 @@
 import { Button, Container } from 'react-bootstrap'
 import { Formik, Form, Field } from 'formik'
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object({
+    name: Yup.string()
+        .required('*Name is required'),
+    email: Yup.string()
+        .required('*Email is required'),
+    password: Yup.string()
+        .min(8, 'Your password is too short!')
+        .required('*Password is required')
+});
 
 const CreateAccount = ({ addUser }) => {
 
@@ -16,6 +27,7 @@ const CreateAccount = ({ addUser }) => {
                             email: '',
                             password: '',
                         }}
+                        validationSchema={validationSchema}
                         onSubmit={(values) => {
                             addUser(values)
                             alert('Account successfully created!')
@@ -28,21 +40,29 @@ const CreateAccount = ({ addUser }) => {
                                     type="text"
                                     onChange={formik.handleChange}
                                     value={formik.values.name}
-                                />
+                                    className={formik.errors.name ? "error-highlight" : null} />
+                                {formik.errors.name ?
+                                    <div className="validation-error">{formik.errors.name}</div> : null}
+
                                 <label className="field" htmlFor="name">Email</label>
                                 <Field
                                     name="email"
                                     type="text"
                                     onChange={formik.handleChange}
                                     value={formik.values.email}
-                                />
+                                    className={formik.errors.email ? "error-highlight" : null} />
+                                {formik.errors.email ?
+                                    <div className="validation-error">{formik.errors.email}</div> : null}
+
                                 <label className="field" htmlFor="password">Password</label>
                                 <Field
                                     name="password"
                                     type="password"
                                     onChange={formik.handleChange}
                                     value={formik.values.password}
-                                />
+                                    className={formik.errors.password ? "error-highlight" : null} />
+                                {formik.errors.password ?
+                                    <div className="validation-error">{formik.errors.password}</div> : null}
                                 <Button type="submit">Create Account</Button>
                             </Form>
                         )
