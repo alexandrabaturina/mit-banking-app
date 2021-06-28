99dd93cfb34c3830d5d287f771e8f1c41b0b3ca1
alexandrabaturina <alexandra.v.baturina@gmail.com>
2021-06-28 12:26:04 -0700

    feat: add hover effect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,40 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Tooltip, OverlayTrigger } from 'react-bootstrap';
 
 const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light" >
-            <Link className="navbar-brand" to="/">Bad Bank</Link>
+            <OverlayTrigger
+                placement="bottom"
+                overlay={<Tooltip id="button-tooltip">Home</Tooltip>}>
+                <Link className="navbar-brand" to="/">Bad Bank</Link>
+            </OverlayTrigger>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
-                    <Link className="nav-item nav-link" to="/createaccount">Create Account</Link>
-                    <Link className="nav-item nav-link" to="/deposit">Deposit</Link>
-                    <Link className="nav-item nav-link" to="/withdraw">Withdraw</Link>
-                    <Link className="nav-item nav-link" to="/alldata">All Data</Link>
+                    <OverlayTrigger
+                        placement="bottom"
+                        overlay={<Tooltip id="button-tooltip">Create an account</Tooltip>}>
+                        <Link className="nav-item nav-link" to="/createaccount">Create Account</Link>
+                    </OverlayTrigger>
+                    <OverlayTrigger
+                        placement="bottom"
+                        overlay={<Tooltip id="button-tooltip">Make a deposit</Tooltip>}>
+                        <Link className="nav-item nav-link" to="/deposit">Deposit</Link>
+                    </OverlayTrigger>
+                    <OverlayTrigger
+                        placement="bottom"
+                        overlay={<Tooltip id="button-tooltip">Withdraw money</Tooltip>}>
+                        <Link className="nav-item nav-link" to="/withdraw">Withdraw</Link>
+                    </OverlayTrigger>
+                    <OverlayTrigger
+                        placement="bottom"
+                        overlay={<Tooltip id="button-tooltip">View all user data</Tooltip>}>
+                        <Link className="nav-item nav-link" to="/alldata">All Data</Link>
+                    </OverlayTrigger>
                 </div>
             </div>
         </nav>
