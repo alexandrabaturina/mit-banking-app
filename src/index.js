499dc63908d5d43508eaf8e42f55c8a009721a68
alexandrabaturina <alexandra.v.baturina@gmail.com>
2021-06-29 14:06:08 -0700

    chore: import styles

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.css'
+import './App.css'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
