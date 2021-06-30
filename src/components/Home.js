f0ac6d93bb5a45dc33fd9aec6b23e5f4beafb8c9
alexandrabaturina <alexandra.v.baturina@gmail.com>
2021-06-29 20:22:13 -0700

    language: fix minor errors

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,10 @@ const Home = () => {
         <Container>
             <div className="card border-secondary text-center">
                 <div className="card-header border-secondary">
-                    <h5 className="card-title">WELCOME TO THE BANK APP</h5>
+                    <h5 className="card-title">WELCOME TO BAD BANK APP</h5>
                 </div>
                 <div className="card-body">
-                    <p className="card-text">The Bank App is here to meet all your banking needs!</p>
+                    <p className="card-text">Bad Bank App is here to meet all your banking needs!</p>
                     <img src='bank.png' className='img-fluid' alt='Bad Bank' />
                 </div>
             </div>
