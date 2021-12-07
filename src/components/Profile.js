8800dcec344b5e8ad228eb62bcc661d4a0f7750a
alexandrabaturina <alexandra.v.baturina@gmail.com>
2021-12-07 12:15:34 -0800

    feat: handle no data

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,38 +7,44 @@ const Profile = ({ users }) => {
 
     return (
         <Container>
-            <div className="card border-secondary text-center all-data-card">
-                <div className="card-header border-secondary">
-                    <h5 className="card-title">Operations of {username}</h5>
+            {!users[username]
+                ? <div>User {username} does not exist.</div>
+                : <div className="card border-secondary text-center all-data-card">
+                    <div className="card-header border-secondary">
+                        <h5 className="card-title">Operations of {username}</h5>
+                    </div>
+                    <div className="card-body">
+                        {users[username].history.length === 0
+                            ? <p className="card-text">No user activity yet.</p>
+                            : <table className="table" verticalalign='middle'>
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Operation</th>
+                                        <th scope="col">Sum, $</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {users[username].history.map((item, index) => (
+                                        <tr key={index}>
+                                            {Object.keys(item)[0] === 'deposit'
+                                                ? <td>Deposit</td>
+                                                : <td>Withdraw</td>
+                                            }
+                                            <td>{Object.values(item)}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <th>BALANCE</th>
+                                        <th>{users[username].balance}</th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        }
+                    </div>
                 </div>
-                <div className="card-body">
-                    <table className="table" verticalalign='middle'>
-                        <thead>
-                            <tr>
-                                <th scope="col">Operation</th>
-                                <th scope="col">Sum, $</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {users[username].history.map((item, index) => (
-                                <tr key={index}>
-                                    {Object.keys(item)[0] === 'deposit'
-                                        ? <td>Deposit</td>
-                                        : <td>Withdraw</td>
-                                    }
-                                    <td>{Object.values(item)}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <th>BALANCE</th>
-                                <th>{users[username].balance}</th>
-                            </tr>
-                        </tfoot>
-                    </table>
-                </div>
-            </div>
+            }
         </Container >
     )
 }
